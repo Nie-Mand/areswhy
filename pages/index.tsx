bcd87491c81041f7f4116ba3fd4caaa7d075d9ea
Nie-Mand <chill.out.x19@gmail.com>
2022-01-16 19:12:20 +0100

    boo:

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <div>
+      <div className="bg-black h-screen">
+        <div
+          className={`absoulte -top-30 blur-[100px] mx-auto 
+          rounded-full h-96 w-96 bg-[radial-gradient(var(--tw-gradient-stops))]
+          from-white/20 via-white/10 to-transparent
+        `}
+        ></div>
+      </div>
+    </div>
+  );
+}
